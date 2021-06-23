36d340c392dd1ad67cd031fb46088dc6633207cc
Regis Santos <regis42santos@gmail.com>
2021-06-23 01:02:51 -0300

    Adiciona chamados. close #11

--- backend/call/urls.py
+++ backend/call/urls.py
@@ -7,7 +7,7 @@ app_name = 'call'
 call_patterns = [
     path('', v.CallListView.as_view(), name='call_list'),
     # path('<int:pk>/', v.call_detail, name='call_detail'),
-    # path('create/', v.CallCreateView.as_view(), name='call_create'),
+    path('create/', v.CallCreateView.as_view(), name='call_create'),
     path('<int:pk>/update/', v.CallUpdateView.as_view(), name='call_update'),
 ]
 
--- backend/call/views.py
+++ backend/call/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin as LRM
-from django.views.generic import ListView, UpdateView
+from django.views.generic import CreateView, ListView, UpdateView
 
 from backend.core.mixins import TotalItems
 
@@ -11,6 +11,11 @@ from .models import Call
 class CallListView(LRM, TotalItems, SearchCallMixin, ListView):
     model = Call
     paginate_by = 20
+
+
+class CallCreateView(LRM, CreateView):
+    model = Call
+    form_class = CallForm
 
 
 class CallUpdateView(LRM, UpdateView):
