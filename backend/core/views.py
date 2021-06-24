5771f57601c69bfff2d5cba8eb9bb6e9c537dbc7
Regis Santos <regis42santos@gmail.com>
2021-06-24 10:40:01 -0300

    Número de chamados por status no dashboard. close #42

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,8 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from backend.call.constants import CONCLUDED, IN_PROGRESS, OPEN
+from backend.call.models import Call
+
 
 @login_required
 def index(request):
     template_name = 'index.html'
-    return render(request, template_name)
+
+    queryset = Call.objects.all()
+    open_status = queryset.filter(status=OPEN).count()
+    in_progress_status = queryset.filter(status=IN_PROGRESS).count()
+    concluded_status = queryset.filter(status=CONCLUDED).count()
+
+    context = {
+        'open_status': open_status,
+        'in_progress_status': in_progress_status,
+        'concluded_status': concluded_status,
+    }
+    return render(request, template_name, context)
